906a1e8b1fb68a0c91c27ff8ffb7c22542c06fbd
barkhayot <barkhayotoff@gmail.com>
2024-11-25 00:44:47 +0900

    fix: move log generators to separate file

--- cmd/humanlog/genny.go
+++ cmd/humanlog/genny.go
@@ -141,39 +141,6 @@ func emitLog(out io.Writer, now time.Time, src rand.Source, format string) error
 	return err
 }
 
-func generateLogfmtLog(now time.Time, src rand.Source) string {
-	return fmt.Sprintf(
-		"time=%s level=%s msg=%q user=%s org=%s",
-		now.Format(time.RFC3339),
-		randel(src, []string{"INFO", "DEBUG", "WARN", "ERROR"}),
-		randel(src, nouns)+" "+randel(src, adjectives),
-		genString(src, false),
-		genString(src, false),
-	)
-}
-
-func generateJSONLog(now time.Time, src rand.Source) string {
-	return fmt.Sprintf(
-		`{"time":"%s","level":"%s","message":"%s","user":"%s","org":"%s"}`,
-		now.Format(time.RFC3339),
-		randel(src, []string{"INFO", "DEBUG", "WARN", "ERROR"}),
-		randel(src, nouns)+" "+randel(src, adjectives),
-		genString(src, false),
-		genString(src, false),
-	)
-}
-
-func generateOtelLog(now time.Time, src rand.Source) string {
-	return fmt.Sprintf(
-		`{"time":"%s","severity":"%s","body":"%s","attributes":{"user":"%s","org":"%s"}}`,
-		now.Format(time.RFC3339Nano),
-		randel(src, []string{"INFO", "DEBUG", "WARN", "ERROR"}),
-		randel(src, nouns)+" "+randel(src, adjectives),
-		genString(src, false),
-		genString(src, false),
-	)
-}
-
 func emitMessage(out io.Writer, now time.Time, src rand.Source) error {
 	t := ts(now, src)
 	l := lvl(src)
